29745bbf0f8d1b11ccde243a796ace7375ad17ed
Phichaiyut026 <sinaumza@gmail.com>
2021-12-02 16:31:55 +0700

    show

--- showdemo.php
+++ showdemo.php
@@ -6,8 +6,8 @@ $dbname = "it64309010011";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-mysqli_set_charset($conn,"utf8");
 // Check connection
+mysqli_set_charset($conn,"utf8");
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
