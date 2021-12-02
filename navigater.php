2e867e7def3cb6294cbeae33751071f9630eba27
Phichaiyut026 <sinaumza@gmail.com>
2021-12-02 11:19:31 +0700

    new5

--- navigater.php
+++ navigater.php
@@ -6,7 +6,7 @@
 
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
 <div class="container-fluid">
-<a class="navbar-brand" href="#">Navbar</a>
+<a class="navbar-brand" href="index.php">HOME</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -15,22 +15,23 @@
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="#">MAC</a>
+        <a class="nav-link" href="page1.php">MAC</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Ipad</a>
+        <a class="nav-link" href="page2.php">Ipad</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Iphone</a>
+        <a class="nav-link" href="page3.php">Iphone</a>
       </li>
         <li class="nav-item">
-        <a class="nav-link" href="#">Watch</a>
+        <a class="nav-link" href="page4.php">Watch</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">TV</a>
+        <a class="nav-link" href="page5.phh">TV</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Music</a>
+        <a class="nav-link" href="page6.php 
+        ">Music</a>
       </li>
 </ul>
   </div>  
